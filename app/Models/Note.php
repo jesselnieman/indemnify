ee6e836171955064ca621de63bec9abdf3873cfb
Jesse Nieman <jesselnieman@gmail.com>
2021-02-22 16:28:10 -0500

    added Note relationship to User and Customer

--- app/Models/Note.php
+++ app/Models/Note.php
@@ -8,4 +8,16 @@ use Illuminate\Database\Eloquent\Model;
 class Note extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['user_id', 'message_text'];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function customer()
+    {
+        return $this->belongsTo(Customer::class);
+    }
 }
